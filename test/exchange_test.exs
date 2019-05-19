2179e223aea8e88ecb73212819e92f036d073275
Zacck Osiemo <coderv63@gmail.com>
2019-05-19 17:05:50 +0200

    Test that send instructions captures and validates instructions well

--- test/exchange_test.exs
+++ test/exchange_test.exs
@@ -2,26 +2,24 @@ defmodule ExchangeTest do
   use ExUnit.Case
   doctest Exchange
 
-
   alias Exchange.{
     Entry,
     State
   }
 
-  setup do 
+  setup do
     {:ok, pid} = Exchange.start_link([])
 
     {:ok, exchange: pid}
-  end 
+  end
 
-  describe "Exchange Server" do 
+  describe "Exchange Server" do
     test "send_instruction/2 returns {:ok} for valid event", %{exchange: pid} do
-  
       event = %{
         instruction: :new,
         side: :ask,
-        price_level_index: 1, 
-        price: 40.9, 
+        price_level_index: 1,
+        price: 40.9,
         quantity: 5
       }
 
@@ -29,8 +27,8 @@ defmodule ExchangeTest do
 
       %{events: [entry]} = :sys.get_state(pid)
 
-      assert %Entry{} = entry 
-    end 
+      assert %Entry{} = entry
+    end
 
     test "unspecified price_level_index in an event is set to 0 in an entry", %{exchange: pid} do
       event = %{
@@ -40,11 +38,72 @@ defmodule ExchangeTest do
         quantity: 6
       }
 
-      {:ok}  = Exchange.send_instruction(pid, event) 
+      {:ok} = Exchange.send_instruction(pid, event)
 
-      %{events: [entry]} = :sys.get_state(pid) 
-      
-      assert entry.price_level_index == 0 
+      %{events: [entry]} = :sys.get_state(pid)
+
+      assert entry.price_level_index == 0
+    end
+
+    test "send_instruction/2 updates the most recent price_level", %{exchange: pid} do
+      event = %{
+        instruction: :new,
+        side: :ask,
+        price_level_index: 1,
+        price: 40.9,
+        quantity: 5
+      }
+
+      {:ok} = Exchange.send_instruction(pid, event)
+
+      %{events: [entry]} = :sys.get_state(pid)
+
+      assert entry.side == event.side
+      assert entry.quantity == event.quantity
+
+      update_event = %{
+        instruction: :update,
+        side: :ask,
+        price_level_index: 1,
+        price: 55.3,
+        quantity: 7
+      }
+
+      {:ok} = Exchange.send_instruction(pid, update_event)
+
+      %{events: [updated_entry]} = :sys.get_state(pid)
+
+      refute updated_entry.price == entry.price
+      refute updated_entry.quantity == entry.quantity
+
+      assert updated_entry.price == update_event.price
+      assert updated_entry.quantity == update_event.quantity
+    end
+
+    test "send_instruction/2 errors out if a price_level_index does not exist", %{exchange: pid} do 
+      event = %{
+        instruction: :new,
+        side: :ask, 
+        price_level_index: 1,
+        price: 50.99,
+        quantity: 7 
+      }
+
+      {:ok}  = Exchange.send_instruction(pid, event)
+
+      assert %{events: [_]} = :sys.get_state(pid)
+
+      update_event = %{
+        instruction: :update,
+        side: :ask,
+        price_level_index: 22,
+        price: 65.7,
+        quantity: 9
+      }
+
+      {:error, reason: _rsn}  = Exchange.send_instruction(pid, update_event)
+
+     assert %{events: [_]} = :sys.get_state(pid)
     end 
-  end 
+  end
 end
