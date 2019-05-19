fc80c7e4dc4e7715d74247c2a1c67e2cedd85b59
Zacck Osiemo <coderv63@gmail.com>
2019-05-19 19:01:53 +0200

    Test delete instruction
    - run formatter on source

--- test/exchange_test.exs
+++ test/exchange_test.exs
@@ -1,10 +1,10 @@
 defmodule ExchangeTest do
   use ExUnit.Case
   doctest Exchange
+  import Exchange.Factory
 
   alias Exchange.{
-    Entry,
-    State
+    Entry
   }
 
   setup do
@@ -15,14 +15,7 @@ defmodule ExchangeTest do
 
   describe "Exchange Server" do
     test "send_instruction/2 returns {:ok} for valid event", %{exchange: pid} do
-      event = %{
-        instruction: :new,
-        side: :ask,
-        price_level_index: 1,
-        price: 40.9,
-        quantity: 5
-      }
-
+      event = build(:entry, %{instruction: :new})
       {:ok} = Exchange.send_instruction(pid, event)
 
       %{events: [entry]} = :sys.get_state(pid)
@@ -80,16 +73,16 @@ defmodule ExchangeTest do
       assert updated_entry.quantity == update_event.quantity
     end
 
-    test "send_instruction/2 errors out if a price_level_index does not exist", %{exchange: pid} do 
+    test "send_instruction/2 errors out if a price_level_index does not exist", %{exchange: pid} do
       event = %{
         instruction: :new,
-        side: :ask, 
+        side: :ask,
         price_level_index: 1,
         price: 50.99,
-        quantity: 7 
+        quantity: 7
       }
 
-      {:ok}  = Exchange.send_instruction(pid, event)
+      {:ok} = Exchange.send_instruction(pid, event)
 
       assert %{events: [_]} = :sys.get_state(pid)
 
@@ -101,9 +94,43 @@ defmodule ExchangeTest do
         quantity: 9
       }
 
-      {:error, reason: _rsn}  = Exchange.send_instruction(pid, update_event)
+      {:error, reason: _rsn} = Exchange.send_instruction(pid, update_event)
 
-     assert %{events: [_]} = :sys.get_state(pid)
-    end 
+      assert %{events: [_]} = :sys.get_state(pid)
+    end
+
+    @delete_num 4
+    @top_num 7 
+    test "send_instruction/2 deletes a price_level", %{exchange: pid} do
+      events =
+        Enum.map(1..@top_num, fn n ->
+          build_list(n, :entry, %{price_level_index: n, instruction: :new})
+        end)
+        |> Enum.reduce([], fn entries, acc -> entries ++ acc end)
+
+      _ = Enum.map(events, fn event -> Exchange.send_instruction(pid, event) end)
+
+      %{events: evs} = :sys.get_state(pid)
+
+      assert Enum.count(events) == Enum.count(evs)
+
+      event = %{
+        instruction: :delete,
+        price_level_index: @delete_num 
+      }
+
+      {:ok} = Exchange.send_instruction(pid, event) 
+
+      %{events: new_evs} = :sys.get_state(pid) 
+      assert Enum.count(evs) - Enum.count(new_evs) == @delete_num
+
+      %{price_level_index: top} =
+        new_evs
+        |> Enum.sort(fn a, b -> a.price_level_index < b.price_level_index end)
+        |> Enum.reverse()
+        |> hd()
+
+      assert top == @top_num - 1 
+    end
   end
 end
